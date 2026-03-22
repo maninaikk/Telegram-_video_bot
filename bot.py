b1ad88793613721ae21c7b5e62fc015839a4b4a7
maninaikk <maniimaniiikanta@gmail.com>
2026-03-22 20:25:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 import yt_dlp
 import os
 
-BOT_TOKEN = os.environ.get("8378656167:AAGn-rIAinArZB0ZcfYhax8poPkCjzf6yH0)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
@@ -15,21 +14,4 @@ async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
     }
 
     try:
-        await update.message.reply_text("Downloading... ⏳")
-
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info)
-
-        with open(filename, "rb") as video:
-            await update.message.reply_video(video=video)
-
-        os.remove(filename)
-
-    except Exception as e:
-        await update.message.reply_text("Error 😔")
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download_video))
-
-app.run_polling()
+        await update.message.reply_text("Downloading...

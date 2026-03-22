6b5a7d4ece1ad485b2e7f5a9cefa70b1e20e5418
maninaikk <maniimaniiikanta@gmail.com>
2026-03-22 19:53:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
-first upload
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+import yt_dlp
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': 'video.%(ext)s'
+    }
+
+    try:
+        await update.message.reply_text("Downloading... ⏳")
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+
+        await update.message.reply_video(video=open(filename, 'rb'))
+
+        os.remove(filename)
+
+    except:
+        await update.message.reply_text("Failed 😔")
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download_video))
+
+app.run_polling()
